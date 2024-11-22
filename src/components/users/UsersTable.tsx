1a8fb40b1759a2783335edd5886762b654ff491a
Eugene Krokhmal <eugene.krokhmal@cloudflight.io>
2024-11-22 22:53:03 +0100

    upd totals

--- src/components/users/UsersTable.tsx
+++ src/components/users/UsersTable.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import Price from '../Price';
 import UserIcon from '../elements/UserIcon';
 import {
-    calculateTotalPaidByUser
+    getInvolvedExpenses,
 } from '../../utils/tripUtils';
 
 interface UsersTableProps {
@@ -26,23 +26,6 @@ const UsersTable: React.FC<UsersTableProps> = ({
     expenses,
 }) => {
     const { t } = useTranslation();
-
-    // Helper function to get the list of expense names for which the user is responsible
-    const getResponsibleExpensesNames = (userId: string) => {
-        return expenses
-            .filter((expense) => expense.responsibleUserId === userId)
-            .map((expense) => expense.expenseName);
-    };
-
-    // Helper function to get the list of expenses the user is involved in (with amounts)
-    const getInvolvedExpenses = (userId: string) => {
-        return expenses
-            .filter((expense) => expense.splitParticipants.includes(userId))
-            .map((expense) => ({
-                name: expense.expenseName,
-                amount: (expense.amount / expense.splitParticipants.length), // Split amount among participants
-            }));
-    };
 
     return (
         <div className="flex flex-col">
@@ -97,29 +80,20 @@ const UsersTable: React.FC<UsersTableProps> = ({
                             <td className="px-2 py-4 text-xs align-top md:w-full">
                                 {/* Display the names and amounts of expenses the user is involved in */}
                                 <ul className="leaders">
-                                    {getInvolvedExpenses(participant._id).length > 0 ? (
+                                    {getInvolvedExpenses(expenses, participant._id).length > 0 ? (
                                         <>
-                                            {getInvolvedExpenses(participant._id).map((expense, index) => (
+                                            {getInvolvedExpenses(expenses, participant._id).map((expense, index) => (
                                                 <li key={index} className="flex justify-between">
                                                     <span className="mr-auto bg-white dark:bg-zinc-800">{expense.name}</span>
                                                     <span className="bg-white dark:bg-zinc-800"><Price price={+expense.amount.toFixed(2)} /></span>
                                                 </li>
                                             ))}
 
-                                            {/* Calculate and display the total */}
-                                            <li className="flex justify-between font-bold mt-2">
-                                                <span className="mr-auto bg-white dark:bg-zinc-800">Total Paid</span>
-                                                <span className="bg-white dark:bg-zinc-800">
-                                                    <Price
-                                                        price={+calculateTotalPaidByUser(expenses, participant._id).toFixed(2)}
-                                                    />
-                                                </span>
-                                            </li>
-                                            <li className="flex justify-between font-bold text-zinc-900 dark:text-zinc-100">
+                                            <li className="flex justify-between font-bold text-zinc-900 dark:text-zinc-100 mt-6">
                                                 <span className="mr-auto bg-white dark:bg-zinc-800">Total Share</span>
                                                 <span className="bg-white dark:bg-zinc-800">
                                                     <Price
-                                                        price={+getInvolvedExpenses(participant._id).reduce((total, expense) => total + expense.amount, 0).toFixed(2)}
+                                                        price={+getInvolvedExpenses(expenses, participant._id).reduce((total, expense) => total + expense.amount, 0).toFixed(2)}
                                                     />
                                                 </span>
                                             </li>
