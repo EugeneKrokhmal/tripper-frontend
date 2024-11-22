51fd25bd1e2a59630dce1e0adbd5f01103fa46ae
Eugene Krokhmal <eugene.krokhmal@cloudflight.io>
2024-11-22 22:55:31 +0100

    upd utils

--- src/utils/tripUtils.ts
+++ src/utils/tripUtils.ts
@@ -3,9 +3,13 @@
 import axios from 'axios';
 
 export const calculateTotalPaidByUser = (expenses: any[], userId: string): number => {
-    return expenses
-        .filter(expense => expense.responsibleUserId === userId)
-        .reduce((total, expense) => total + expense.amount, 0);
+    return expenses.reduce((total, expense) => {
+        if (expense.splitParticipants.includes(userId)) {
+            const userShare = expense.amount / expense.splitParticipants.length;
+            return total + userShare;
+        }
+        return total;
+    }, 0);
 };
 
 export const calculateTotalCost = (expenses: any[]): number => {
@@ -38,4 +42,21 @@ export const fetchCityImage = async (destination: string, UNSPLASH_ACCESS_KEY: s
     }
 
     return null;
+};
+
+// Helper function to get the list of expense names for which the user is responsible
+export const getResponsibleExpensesNames = (expenses: any[], userId: string) => {
+    return expenses
+        .filter((expense) => expense.responsibleUserId === userId)
+        .map((expense) => expense.expenseName);
+};
+
+// Helper function to get the list of expenses the user is involved in (with amounts)
+export const getInvolvedExpenses = (expenses: any[], userId: string) => {
+    return expenses
+        .filter((expense) => expense.splitParticipants.includes(userId))
+        .map((expense) => ({
+            name: expense.expenseName,
+            amount: (expense.amount / expense.splitParticipants.length), // Split amount among participants
+        }));
 };
